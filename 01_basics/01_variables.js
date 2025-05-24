4d0c6e29bca2c9881c8788fe7a8997d37b74f51e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-05-24 10:37:06 +0000

    learned about let, var and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 3432523
+let accountEmail = "anirudh.ag.gupta@oracle.com"
+let accountPassword = "12345"
+let accountState
+
+accountCity = "Jaipur"
+
+// accountId = 2         Not Allowed
+
+accountEmail = "ag@ag.com"
+accountPassword = "342342"
+accountCity = "Hyderabad"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Variables and Data Types
+// const = can't be re-assigned a value and can't be declared again  (block scope)
+// let = can be re-assigned a value but can't be declared again (block scope)
+// var = it can be re-assigned a value and it can also be declared again  (Global scope)
