6c9e090605c70f4467d67036c09efa88c02599ee
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-05-24 11:07:46 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3); we are using node js not browser , it works when js engine is binded with browser
+
+let flag = true;
+let age = 25;
+let name = "Anirudh";
+
+//number 
+//string
+//boolean
+// null = stand alone value (type = object)
+// undefined = kindda place holder for a value which is not defined yet (type = undefined)
+// symbol = uniquness
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
