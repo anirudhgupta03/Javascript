b8fb3adc4633f2b00b4fdd4f652e43ad35f9697a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-06-14 11:32:47 +0000

    object basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,45 @@
+// singleton
+
+// object literals
+// Object.create
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name: "Anirudh",
+    age: 25,
+    location: "Jaipur",
+    email: "anirudhkgupta222@gmail.com",            
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"],
+    "fullName": "Anirudh Gupta",
+    [mySym]: "mykey1"
+}
+console.log(JsUser.email)
+console.log(JsUser['email'])
+console.log(JsUser.fullName)        //Anirudh Gupta
+console.log(JsUser[mySym])
+
+JsUser.email = "anirudh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "anirudh@microsoft.com"
+console.log(JsUser)
+
+// {
+//   name: 'Anirudh',
+//   age: 25,
+//   location: 'Jaipur',
+//   email: 'anirudh@chatgpt.com',
+//   isLoggedIn: false,
+//   lastLoginDays: [ 'Monday', 'Saturday' ],
+//   fullName: 'Anirudh Gupta',
+//   [Symbol(key1)]: 'mykey1'
+// }
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello JS user, ${this.name }`)
+}
+JsUser.greeting()
