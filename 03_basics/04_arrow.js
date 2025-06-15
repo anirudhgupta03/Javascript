efd3a80363562d892c7f6baf9f65eb5c20a0971f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-06-15 11:56:45 +0000

    arrow functions

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,71 @@
+const user = {
+    username: "anirudh",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this)
+    }
+}
+
+user.welcomeMessage()       //anirudh , welcome to website
+user.username = "hitesh"
+user.welcomeMessage()       //hitesh , welcome to website
+
+console.log(this)       //node environment {}
+
+// Previously the engine used to run js was present only
+// in browser, but in current time, we have various engines to run js
+// like node, deno etc.
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this)
+//     console.log(this.username)  //undefined
+// }
+
+// chai()
+/*
+<ref *1> Object [global] {
+  global: [Circular *1],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  structuredClone: [Function: structuredClone],
+  atob: [Function: atob],
+  btoa: [Function: btoa],
+  performance: [Getter/Setter],
+  fetch: [Function: fetch],
+  navigator: [Getter],
+  crypto: [Getter]
+}
+*/
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username)      //undefined
+// }
+
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this)               //{}
+//     console.log(this.username)      //undefined
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+// const addTwo = (num1, num2) => num1 + num2      //7
+// const addTwo = (num1, num2) => (num1 + num2)    //7 if we use curly braces then we. have to add return statement 
+// const addTwo = (num1, num2) => ({username: "anirudh"})
+
+// console.log(addTwo(3, 4))
