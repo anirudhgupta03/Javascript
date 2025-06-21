72805fe6c4e1e8419e103d58455c0337ae8d44ba
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-06-21 10:53:07 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 0
+
+while(index <= 10){
+    console.log(`Value of index is ${index}`)
+    index++
+}
+
+let myArray = ['flash', 'batman', 'superman']
+
+let ind = 0;
+while(ind < myArray.length){
+    console.log(`Value is ${myArray[ind]}`);
+    ind++;
+}
+
+let score = 11
+do{
+    console.log(`Score is ${score}`)
+}while(score <= 10);
