cb14e277b421de2d8773a503ada69b29fc28cd85
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-05 14:41:03 +0530

    Update setup.js

--- 01_basics/setup.js
+++ 01_basics/setup.js
@@ -3,4 +3,6 @@
 
 //For long time, this compiler was hidden in a browser
 //but now Node.js, Deno.js executes JS files
+
+//Node.js is an open source, cross platform, javascript runtime environment
 console.log("Hitesh")
